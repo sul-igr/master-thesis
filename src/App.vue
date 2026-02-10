ec92cdb2179f86a1072e411fa1235eeb6dffeff1
darklight81 <igor@yolkstudio.com>
2026-02-10 16:33:42 +0100

    feat: add social login

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,8 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import MainHeader from './components/MainHeader.vue'
-import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
-import { createAppKit } from '@reown/appkit/vue'
-import { anvil, arbitrum, mainnet, type AppKitNetwork } from '@reown/appkit/networks'
-
-const projectId = 'e1437b0fb1674059c37d8a6e3808e4f0'
-
-const metadata = {
-  name: 'SubEth',
-  description: 'EIP-7702 Recurring Payments',
-  url: 'https://localhost:3000',
-  icons: ['https://avatars.githubusercontent.com/u/179229932'],
-}
-
-const networks: [AppKitNetwork, ...AppKitNetwork[]] = [anvil, arbitrum, mainnet]
-
-const wagmiAdapter = new WagmiAdapter({
-  networks,
-  projectId,
-})
-
-createAppKit({
-  adapters: [wagmiAdapter],
-  networks,
-  projectId,
-  metadata,
-  features: {
-    analytics: true,
-  },
-})
+// Import wagmi config to ensure AppKit is initialized before the app renders
+import '@/config/wagmi'
 </script>
 
 <template>
