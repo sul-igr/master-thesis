b8e10d1860d7ebf3bff23f46734f950d270f22f4
darklight81 <igor@yolkstudio.com>
2026-02-12 22:26:05 +0100

    feat: add be url

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,4 @@
 export const ACCOUNT_IMPLEMENTATION_ADDRESS = '0x5FbDB2315678afecb367f032d93F642f64180aa3' as const
 
 /** Backend API base URL (used by manually-written API helpers) */
-export const API_BASE_URL = 'http://localhost:3001' as const
+export const API_BASE_URL = 'https://thesis-be-p2x2.onrender.com' as const
