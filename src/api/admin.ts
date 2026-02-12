9e96878faf41e873a2ea3b39e8594392b0c4c125
darklight81 <igor@yolkstudio.com>
2026-02-12 23:14:22 +0100

    fix: fix type

--- src/api/admin.ts
+++ src/api/admin.ts
@@ -4,7 +4,7 @@ export const checkIsAdmin = async (address: string): Promise<boolean> => {
   try {
     const res = await getApiAdminsCheckAddress(address)
     if (res.status !== 200) return false
-    const data = res.data as { isAdmin?: boolean }
+    const data = res.data as unknown as { isAdmin?: boolean }
     return data?.isAdmin === true
   } catch {
     return false
